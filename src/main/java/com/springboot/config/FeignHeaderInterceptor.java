062978308b9f723546044bf3c2ef83d2ba7f1ab8
chenbinJ <1358369360@qq.com>
2020-05-12 15:28:30 +0800

    沭阳添加feign的请求地址打印

--- src/main/java/com/springboot/config/FeignHeaderInterceptor.java
+++ src/main/java/com/springboot/config/FeignHeaderInterceptor.java
@@ -12,6 +12,7 @@ public class FeignHeaderInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
+        log.info("log-feign 请求的地址为："+requestTemplate.url());
         log.info("log-feign 请求的参数为："+requestTemplate.requestBody().asString());
 //        System.out.println("out 请求的参数为："+requestTemplate.requestBody().asString());
     }
