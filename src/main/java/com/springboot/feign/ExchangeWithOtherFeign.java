9581bc8f3d08bcd040980daae7bdbc1b6c72e646
ShiKe <446705520@qq.com>
2020-06-28 16:43:45 +0800

    住建api修改

--- src/main/java/com/springboot/feign/ExchangeWithOtherFeign.java
+++ src/main/java/com/springboot/feign/ExchangeWithOtherFeign.java
@@ -14,7 +14,7 @@ public interface ExchangeWithOtherFeign {
     @RequestMapping(value = "${Feign.Tax}",method = RequestMethod.POST,produces = "application/json",consumes = "application/json;charset=UTF-8",headers = {"api_id=3ycpo7Cc","from_user=0b6c220cdfc54288b6630eb1a7fa612f"})
     ObjectRestResponse<String> testTax(Map<String,Object> taxParamBody);
 
-    @RequestMapping(value = "${Feign.Tra}",method = RequestMethod.POST,produces = "application/json",consumes = "application/json;charset=UTF-8",headers = {"api_id=qYvkPWSr","from_user=0b6c220cdfc54288b6630eb1a7fa612f"})
+    @RequestMapping(value = "${Feign.Tra}",method = RequestMethod.POST,produces = "application/json",consumes = "application/json;charset=UTF-8",headers = {"api_id=q8Ky2gtx","from_user=1"})
     ObjectRestResponse<String> testTra(Map<String,Object> TraParamBody);
 
     /**
