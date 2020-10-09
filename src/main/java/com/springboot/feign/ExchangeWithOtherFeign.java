31252b358ed61032a6d07c00b0411dc66b4bf62f
chenbinJ <1358369360@qq.com>
2020-10-09 16:47:42 +0800

    update

--- src/main/java/com/springboot/feign/ExchangeWithOtherFeign.java
+++ src/main/java/com/springboot/feign/ExchangeWithOtherFeign.java
@@ -13,10 +13,10 @@ import java.util.Map;
 @FeignClient(name = "otherFeign", url = "${other.url}")
 public interface ExchangeWithOtherFeign {
 
-    @RequestMapping(value = "${Feign.Tax}",method = RequestMethod.POST,produces = "application/json",consumes = "application/json;charset=UTF-8",headers = {"api_id=3ycpo7Cc","from_user=0b6c220cdfc54288b6630eb1a7fa612f"})
+    @RequestMapping(value = "${Feign.Tax}",method = RequestMethod.POST,produces = "application/json",consumes = "application/json;charset=UTF-8",headers = {"api_id=dWYiasy7","from_user=1"})
     ObjectRestResponse<String> testTax(Map<String,Object> taxParamBody);
 
-    @RequestMapping(value = "${Feign.Tra}",method = RequestMethod.POST,produces = "application/json",consumes = "application/json;charset=UTF-8",headers = {"api_id=qYvkPWSr","from_user=0b6c220cdfc54288b6630eb1a7fa612f"})
+    @RequestMapping(value = "${Feign.Tra}",method = RequestMethod.POST,produces = "application/json",consumes = "application/json;charset=UTF-8",headers = {"api_id=dWYiasy7","from_user=1"})
     ObjectRestResponse<String> testTra(Map<String,Object> TraParamBody);
 
     /**
